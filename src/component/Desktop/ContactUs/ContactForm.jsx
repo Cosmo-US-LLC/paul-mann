c02daf4d92b6e278794d8159df9659e157fb51cb
nomanqadir-cosmoinc <nomanqadir@cosmoinc.co>
2025-02-06 11:28:24 +0500

    contact-us page

--- src/component/Desktop/ContactUs/ContactForm.jsx
+++ src/component/Desktop/ContactUs/ContactForm.jsx
@@ -3,18 +3,15 @@ import React from "react";
 const ContactForm = () => {
   return (
     <div className="max-w-[1440px] w-full h-full mx-auto">
-      <div className="max-w-[1200px] h-[650px] mx-auto flex flex-col justify-center">
-        <div className="flex flex-col items-start justify-between gap-10 bg-white md:flex-row ">
+      <div className="max-w-[1200px] h-[750px] mx-auto flex flex-col justify-center">
+        <div className="flex flex-col items-start justify-between gap-10 mb-8 bg-white md:flex-row ">
           <div className="w-full space-y-5 md:w-1/2">
             <h2 className="text-[45px] font-[RFDewi-bold] font-[700] capitalize leading-[53px]">
               Still Have Questions?
             </h2>
             <p className="text-[#000] font-[Roboto] font-[400] text-[16px] leading-[23px] w-[330px]">
               Email us:{" "}
-              <a
-                href="mailto:info@waterchampion.com"
-                className="text-blue-500  underline"
-              >
+              <a href="mailto:info@waterchampion.com" className="text-black ">
                 info@waterchampion.com
               </a>
             </p>
@@ -23,29 +20,22 @@ const ContactForm = () => {
               <span className="font-medium">866-3WATERS | 866-392-8372</span>
             </p>
 
-            <form className="space-y-4">
+            <form className="flex flex-col space-y-1">
               <input
                 type="text"
                 placeholder="Name"
-                className="w-full py-2 font-[Roboto] border-b-2 border-gray-400 outline-none focus:border-black"
+                className="w-full py-6  font-[Roboto] border-b-2 border-gray-400 outline-none focus:border-black"
               />
               <input
                 type="email"
                 placeholder="Your Email"
-                className="w-full py-2 font-[Roboto] border-b-2 border-gray-400 outline-none focus:border-black"
+                className="w-full  py-6 font-[Roboto] border-b-2 border-gray-400 outline-none focus:border-black"
               />
               <textarea
                 placeholder="Your Message"
                 rows="4"
-                className="w-full py-2 font-[Roboto] border-b-2 border-gray-400 outline-none focus:border-black"
+                className="w-full  py-6 font-[Roboto] border-b-2 border-gray-400 outline-none focus:border-black"
               />
-              <button
-                type="submit"
-                className="px-6 py-2 mt-4 font-semibold font-[Roboto] text-[#073B63] hover:bg-[#073B63] hover:text-[#FFF] border border-[#073B63] bg-white shadow-inner"
-              
-              >
-                SEND
-              </button>
             </form>
           </div>
 
@@ -57,6 +47,12 @@ const ContactForm = () => {
             />
           </div>
         </div>
+        <button
+          type="submit"
+          className="w-[120px] py-3 font-semibold font-[Roboto] text-[#073B63] hover:bg-[#073B63] hover:text-[#FFF] border border-[#073B63] bg-white shadow-inner"
+        >
+          SEND
+        </button>
       </div>
     </div>
   );
